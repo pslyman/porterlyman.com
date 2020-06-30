cc63b076381836247ff4e38f9e0582c47e6ba73e
Porter Lyman <porter.lyman@healthcatalyst.com>
2020-06-30 11:11:43 -0600

    Working and styled with expanding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Porter Lyman
-      </h1>
-    </div>
-
-
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+  selector: "app-root",
+  templateUrl: `./app.component.html`,
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'PorterLyman';
+export class AppComponent implements OnInit {
+  tiles = [
+    {
+      id: 0,
+      title: "Software Engineer",
+      subTitle: "Health Catalyst",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      big: true,
+      toggled: false,
+    },
+    {
+      id: 1,
+      title: "item2",
+      subTitle: "",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      big: false,
+      toggled: false,
+    },
+    {
+      id: 2,
+      title: "item3",
+      subTitle: "",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      big: false,
+      toggled: false,
+    },
+    {
+      id: 3,
+      title: "item4",
+      subTitle: "",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      big: false,
+      toggled: false,
+    },
+    {
+      id: 4,
+      title: "item5",
+      subTitle: "",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      big: false,
+      toggled: false,
+    },
+    {
+      id: 5,
+      title: "item6",
+      subTitle: "",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      big: false,
+      toggled: false,
+    },
+    {
+      id: 6,
+      title: "item7",
+      subTitle: "",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      big: false,
+      toggled: false,
+    },
+    {
+      id: 7,
+      title: "item8",
+      subTitle: "",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      big: false,
+      toggled: false,
+    },
+    {
+      id: 8,
+      title: "item9",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      big: false,
+      toggled: false,
+    },
+  ];
+
+  title = "Porter Lyman";
+
+  ngOnInit() {}
+
+  tileClick(tileId) {
+    console.log(tileId);
+    this.tiles[tileId].toggled = !this.tiles[tileId].toggled;
+    console.log(this.tiles[tileId].toggled);
+  }
 }
