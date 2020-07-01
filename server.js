f689172e5921ccc10e2f739deb374a6147a4436b
Porter Lyman <porter.lyman@healthcatalyst.com>
2020-07-01 15:50:42 -0600

    6

--- server.js
+++ server.js
@@ -11,10 +11,4 @@ app.get("/*", function (req, res) {
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+app.listen(process.env.PORT || 8080);
