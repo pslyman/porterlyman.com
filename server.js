94b739f8a3f5634ce03205bc0432d16fb970df88
Porter Lyman <porter.lyman@healthcatalyst.com>
2020-07-01 14:59:00 -0600

    testing logging with env.PORT

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/PorterLyman'));
+app.use(express.static(__dirname + "/dist/PorterLyman"));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/PorterLyman/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/PorterLyman/index.html"));
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
