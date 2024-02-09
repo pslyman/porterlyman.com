101a06be51c22560f106b0c356ef640d504d95d2
Porter Lyman <lymanpo@repsrv.com>
2024-02-09 15:14:53 -0700

    Add two more portfolio items

--- src/app/constants/portfolio.constants.ts
+++ src/app/constants/portfolio.constants.ts
@@ -5,4 +5,16 @@ export const portfolioList = [
     url: 'https://www.products.porterlyman.com/PizzaApps',
     description: `An all in one tool built on decades old formulas, this is a mobile recreation of well-known pizza dough tools from pizzamaking.com. Used by thousands of chefs and home cooks, this tool is a must-have for anyone who makes pizza. Quickly input your pizza parameters, and generate a table of ingredients and instructions. It's free to download! If you find yourself needing extra features, the premium version costs as much as a Dr. Pepper at your local pizzeria.`,
   },
+  {
+    id: 1,
+    title: 'porterlyman.com',
+    url: 'https://github.com/pslyman/porterlyman.com',
+    description: `You're here now. A website business card.`,
+  },
+  {
+    id: 2,
+    title: 'products.porterlyman.com',
+    url: 'https://github.com/pslyman/products.porterlyman',
+    description: `Used to manage changelogs and other information for my products.`,
+  },
 ];
